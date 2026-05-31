24d317f0cd7fa07bebd29593f6ff0254aeb2e000
MarcoGarden <marco-vincenzi@hotmail.com>
2026-05-31 12:26:58 +0200

    fix: redirect supabase verification mail

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -24,6 +24,9 @@ export default function Register() {
     const { error } = await supabase.auth.signUp({
       email,
       password,
+      options: {
+    emailRedirectTo: `${window.location.origin}/login`,
+    },
     })
 
     if (error) {
